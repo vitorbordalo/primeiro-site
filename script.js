a7258292aaebc08476ad3706a2d9421a78096be6
vitorbordalo <151081737+vitorbordalo@users.noreply.github.com>
2023-11-24 11:47:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+ const html= document.documentElement
+ html.classList.toggle('ligth')
+ 
+ /* pegar tag img */
+ const img = document.querySelector("#profile img")
+
+ /* substituir a imagem */
+ if(html.classList.contains("ligth")) {
+
+ /* se tiver light mode, adicionar */
+ img.setAttribute("src", "./assets/avatar-light-esse copy.png")  
+ 
+    
+ } else{
+
+  /* se nao tiver light mode, manter */
+  img.setAttribute('src', './assets/avatar.png')  
+ }
+}
+
+
