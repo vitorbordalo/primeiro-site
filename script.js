043809847780859724893bf9283a4e38fed76fbc
vitorbordalo <151081737+vitorbordalo@users.noreply.github.com>
2023-11-26 17:13:13 -0300

    fixing bugs

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 function toggleMode() {
- const html= document.documentElement
- html.classList.toggle('ligth')
+  const html = document.documentElement
+  html.classList.toggle("ligth")
  
  /* pegar tag img */
- const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img")
 
  /* substituir a imagem */
- if(html.classList.contains("ligth")) {
-
- /* se tiver light mode, adicionar */
- img.setAttribute("src", "./assets/avatar-light-esse copy.png")  
- 
-    
- } else{
-
+  if (html.classList.contains("ligth")) {
+  /* se tiver light mode, adicionar */
+  img.setAttribute("src", "./assets/avatar-light-esse.png")  
+  } else {
   /* se nao tiver light mode, manter */
-  img.setAttribute('src', './assets/avatar.png')  
- }
+  img.setAttribute("src", "./assets/Avatar.png")  
+  }
 }
 
 
